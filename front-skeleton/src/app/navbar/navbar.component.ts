35f88dcb97633fcb92ff8d4c342ec75600ed94ae
Paul Mardhel <mardhelpaul@gmail.com>
2024-05-11 01:09:15 +0200

    squash! back_en_cours

--- front-skeleton/src/app/navbar/navbar.component.ts
+++ front-skeleton/src/app/navbar/navbar.component.ts
@@ -12,10 +12,10 @@ export class NavbarComponent {
   constructor() {
 
     this.links.push({name:"Home", href: ""})
-    this.links.push({ name: "Clients", href: "etudiants" })
+    this.links.push({ name: "Client", href: "etudiants" })
     this.links.push({name:"Cours", href: "tester"})
     this.links.push({name:"Tarifs", href: "prix"})
-    this.links.push({name:"Contact", href: "cont"})
+    this.links.push({name:"Mail", href: "cont"})
     this.links.push({name:"Status",href:"statuses"})
   }
 }
